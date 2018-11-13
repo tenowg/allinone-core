0f062688a382bdae9aa21b10b8c07e192809d74b
Craig Russell <tenowg@gmail.com>
2018-11-12 20:49:45 -0500

    character

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -5,14 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use EveSSO\CharacterPublic;
 use EveSSO\EveSSO;
+use App\Traits\Permissions;
 
 class CharacterController extends Controller
 {
+    use Permissions;
+
     public function getCharacterPublic(CharacterPublic $character) {
         return $character;
     }
 
     public function getCharacterProfile(CharacterPublic $character) {
+        $user = \Auth::user();
+
+        if ($user) {
+            if ($user->can('viewStats', $character)) {
+                $character->load('stats');
+            }
+        }
         return $character;
     }
 }
